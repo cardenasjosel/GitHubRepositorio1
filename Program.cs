18b0714e7e69987b840bd841d90b5d74b9d526e7
Jose Cardenas <cardenasjosse83@gmail.com>
2023-06-07 07:00:22 -0600

    Feat: Se agrego el calculo de la poblacion promedio

--- Program.cs
+++ Program.cs
@@ -35,3 +35,5 @@ for (var i = 0; i < Poblacion.Length; i++)
 Console.WriteLine($"Población General:{diccionario.Values.Sum():N0}");
    Console.WriteLine($"Departamento con mayor Población:{maxDepKey}");
    Console.WriteLine($"Departamento con menor Población:{minDepKey}");
+//Poblacion Promedio
+Console.WriteLine($"Poblacion promedi:{diccionario.Values.Average():N2}");
